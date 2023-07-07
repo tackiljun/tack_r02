1f04e7fb8ba7a99224e291dcc691436f759a45c2
tarkIlJun <tack89826@gmail.com>
2023-07-07 14:49:05 +0900

    0707-3

--- src/pages/board/ListPage.js
+++ src/pages/board/ListPage.js
@@ -2,77 +2,60 @@ import { createSearchParams, useNavigate, useSearchParams } from "react-router-d
 import BasicLayout from "../../layouts/BasicLayout";
 import ListComponent from "../../components/board/ListComponent";
 import ListSearchComponent from "../../components/board/ListSearchComponent";
+import useQueryObj from "../../hooks/useQueryObj";
 
 
-const checkNull = (obj) => {
+// const checkNull = (obj) => {
 
-    const result = {}
+//     const result = {}
 
-    for(const attr in obj) {
-        const attrName = attr
-        const attrValue = obj[attr]
+//     for(const attr in obj) {
+//         const attrName = attr
+//         const attrValue = obj[attr]
 
-        if( attrValue && attrValue !== 'null') {
-        result[attrName] = attrValue
-        }   
-    } 
+//         if( attrValue && attrValue !== 'null') {
+//         result[attrName] = attrValue
+//         }   
+//     } 
 
-    return result
-}
+//     return result
+// }
 
 const ListPage = () => {
 
-    const [search, setSearch] = useSearchParams()
-
-    const navigate = useNavigate()
-
-    console.log(search)
-
-    const page = search.get("page") || 1
-    const size = search.get("size") || 10
-    const type = search.get("type")
-    const keyword = search.get("keyword")
-
-    const queryObj = checkNull({page, size, type, keyword})
-
-    console.log("---------------queryObj---------------")
+    const [queryObj, setSearch, moveRead, moveList] = useQueryObj()
+  
+    console.log("queryObj --------")
     console.log(queryObj)
-
+  
     const movePage = (num) => {
-        console.log("----------NUM----------" + num)
-        queryObj.page = num
-        setSearch({...queryObj})
+  
+      console.log("NUM ------------" + num)
+      queryObj.page = num
+      setSearch({...queryObj})
     }
-
-    //console.log("--------------- BOARD LIST ---------------")
+  
     const moveSearch = (type, keyword) => {
-        queryObj.page = 1
-        queryObj.type = type
-        queryObj.keyword = keyword
-
-        setSearch({...queryObj})
-    }
-
-    const moveRead = (bno) => {
-
-        console.log("moveRead: " + bno)
-
-        const queryString = createSearchParams(queryObj).toString()
-
-        navigate(`/board/read/${bno}?${queryString}`)
-
+      queryObj.page = 1
+      queryObj.type = type
+      queryObj.keyword = keyword
+  
+      setSearch({...queryObj})
     }
 
     return ( 
         <div>
-            BOARD LIST PAGE
-            <ListSearchComponent queryObj={queryObj} moveSearch={moveSearch}>
-            </ListSearchComponent>
-
-            <ListComponent queryObj={queryObj} movePage={movePage} moveRead={moveRead}>
-            </ListComponent>
-        </div>
-     );
-}
+          Board List Page
+          <ListSearchComponent moveSearch={moveSearch} queryObj={queryObj}>
+          </ListSearchComponent>
+    
+          <ListComponent 
+          queryObj={queryObj} 
+          movePage = {movePage} 
+          moveRead = {moveRead}
+          ></ListComponent>
+        </div>  
+       );
+    }
  
 export default ListPage;
