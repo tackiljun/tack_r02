232381a115849ec627f0fcdac7e0d6bb16f125df
tarkIlJun <tack89826@gmail.com>
2023-07-14 16:34:13 +0900

    0714- 오후final

--- src/reducers/loginSlice.js
+++ src/reducers/loginSlice.js
@@ -1,23 +1,45 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { getCookie, setCookie } from "../util/cookieUtil";
+
+
+const loadCookie = () => {
+
+    const loginObj = getCookie("login")
+
+    console.log("login..........obj..........")
+    console.log(loginObj)
+
+    if(!loginObj) {
+        return initState
+    }
+    return loginObj
+}
 
 
 const initState = {
-    email:'',
-    signed: false
+    email: '',
+    signed: false  // 체크로그인이라는 함수를 만들 생각.
 }
+
 
 const loginSlice = createSlice({
     name: 'loginSlice',
-    initialState: initState,
+    // 함수의 리턴값으로 받을 생각.
+    initialState: loadCookie(),  
     reducers: {
         requestLogin: (state, param) => {
             const payload = param.payload
             console.log("requestLogin", payload)
 
-            return {email:payload.email, signed:true}
+            const loginObj = {email:payload.email, signed:true}
+
+            setCookie("login", JSON.stringify(loginObj), 1)
+
+            return loginObj
         }
     }
 })
+
 
 export const {requestLogin} = loginSlice.actions
 
---
+++ src/util/cookieUtil.js
@@ -0,0 +1,29 @@
+import { Cookies } from "react-cookie";
+
+
+// 담는것 / 가져오는것.
+const cookies = new Cookies()
+
+
+export const setCookie = (cookieName, value, days) => {
+
+    const expires = new Date()
+    // setUTCDate - 날짜를 조정하는기능.
+    expires.setUTCDate(expires.getUTCDate() + days);
+
+    // 쿠키를 저장하면 옵션이 들어가.
+    cookies.set(cookieName, value, { path: "/", expires: expires})
+    // 로그인을 하면 쿠키값을 저장해야해.
+}
+
+
+export const getCookie = (cookieName) => {
+
+    return cookies.get(cookieName)
+}
+
+
+export const removeCookie = (cookieName, path="/") => {
+
+    cookies.remove(cookieName, {path:path})
+}
